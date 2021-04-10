488e346e11294f959a7581d22e6f1cda9c7123b6
cascadecoder <75092350+cascadecoder@users.noreply.github.com>
2021-04-10 17:43:11 -0500

    Update rowannerd-game-1.js

--- rowannerd-game-1.js
+++ rowannerd-game-1.js
@@ -2,5 +2,5 @@
 //credit to GetCoding! for <canvas> element
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "blue";
+ctx.fillStyle = "rgb(255, 255, 200)";
 ctx.fillRect(0, 0, 300, 300);
