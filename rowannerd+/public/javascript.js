e8cb2561b25df2b77d63c73782409ac70f31ed0b
cascadecoder <75092350+cascadecoder@users.noreply.github.com>
2021-05-04 16:38:24 -0500

    Update javascript.js

--- rowannerd+/public/javascript.js
+++ rowannerd+/public/javascript.js
@@ -1,10 +1,1 @@
-var user = cookie.get("user");
-
-if (user == null || user == "") {
-
-user = prompt("Choose a username:");
-if(!user) {
-alert("We cannot work with you like that! Reload! Now!");
-} else {
-cookie.set("user", user);
-}
+var user = prompt("Username:");
