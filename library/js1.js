5aaf7b2b9ebbf6117ad398f36e3639cf10582445
cascadecoder <75092350+cascadecoder@users.noreply.github.com>
2022-03-31 07:39:15 -0500

    Update js1.js

--- library/js1.js
+++ library/js1.js
@@ -1,13 +1,16 @@
 var a1 = {};
 a1.id = function (i) {
-    return document.getElementById(i);
+	return document.getElementById(i);
 };
 a1.show = function(id) {
-    a1.id(id).style.display = "block";
+	a1.id(id).style.display = "block";
 };
 a1.hide = function(id) {
-    a1.id(id).style.display = "none";
+	a1.id(id).style.display = "none";
 };
-a1.create = function(type, id) {
-    
+a1.create = function(type, id, content) {
+	const elm = document.createElement(type);
+	elm.id = id;
+	elm.innerHTML = content;
+	document.body.appendChild(elm);
 };
