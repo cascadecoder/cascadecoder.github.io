38ff71a8d0e7373ac6a10fe6f311e16115d8fcf1
cascadecoder <75092350+cascadecoder@users.noreply.github.com>
2022-03-31 07:34:56 -0500

    Create js1.js

---
+++ library/js1.js
@@ -0,0 +1,13 @@
+var a1 = {};
+a1.id = function (i) {
+    return document.getElementById(i);
+};
+a1.show = function(id) {
+    a1.id(id).style.display = "block";
+};
+a1.hide = function(id) {
+    a1.id(id).style.display = "none";
+};
+a1.create = function(type, id) {
+    
+};
