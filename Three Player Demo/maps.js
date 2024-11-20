2ca3b4c77d31793a523b07f5859bddb7898a1e44
cascadecoder <75092350+cascadecoder@users.noreply.github.com>
2024-11-20 05:59:57 -0600

    Create maps.js

---
+++ Three Player Demo/maps.js
@@ -0,0 +1,57 @@
+function map1() {
+    addObject(0, 100, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(200, 100, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(400, 100, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(600, 100, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(800, 125, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(1000, 100, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(1200, 100, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(25, 175, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(225, 175, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(425, 175, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(100, 250, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(300, 250, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(25, 325, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(425, 325, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(100, 385, 100, 15, "rgb(200,150,50)", "solid")
+    addObject(350, 385, 100, 15, "rgb(200,150,50)", "solid")
+    addObject(650, 200, 100, 15, "rgb(200,150,50)", "solid")
+    addObject(850, 200, 100, 15, "rgb(200,150,50)", "solid")
+    addObject(500, 250, 150, 20, "rgb(200,150,50)", "solid")
+    addObject(750, 250, 100, 15, "rgb(200,150,50)", "solid")
+    addObject(950, 250, 100, 15, "rgb(200,150,50)", "solid")
+    addObject(25, 450, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(425, 450, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(775, 450, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(1225, 450, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(1110, 200, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(1155, 190, 20, 10, "yellow", "bounce");
+    addObject(1225, 440, 30, 10, "purple", "speed");
+    addObject(880, 190, 30, 10, "purple", "speed");
+    addObject(670, 320, 30, 10, "purple", "speed");
+    addObject(600, 330, 175, 15, "rgb(200,150,50)", "solid")
+    addObject(850, 315, 100, 20, "rgb(200,150,50)", "solid")
+    addObject(1050, 375, 30, 500, "rgb(200,150,50)", "solid")
+    addObject(950, 425, 30, 500, "rgb(200,150,50)", "solid")
+    addObject(165, 240, 20, 10, "yellow", "bounce");
+    addObject(365, 240, 20, 10, "yellow", "bounce");
+    addObject(240, 515, 30, 10, "yellow", "bounce");
+    addObject(565, 240, 30, 10, "yellow", "bounce");
+    addObject(825, 440, 30, 10, "yellow", "bounce");
+    addObject(987.5, 515, 55, 10, "yellow", "bounce");
+    addObject(1100, 515, 30, 10, "yellow", "bounce");
+    addObject(255, 165, 30, 10, "purple", "speed");
+    addObject(50, 515, 30, 10, "purple", "speed");
+    addObject(450, 515, 30, 10, "purple", "speed");
+    addObject(1250, 483, 15, 15, "green", "teleporter1");
+    addObject(480, 30, 15, 15, "green", "teleporter2");
+    addObject(620, 430, 15, 15, "green", "teleporter3");
+}
+
+function map2() {
+
+}
+
+function map3() {
+    
+}
